bb0ac91046e5e16de4ca3652e4d95920616cf287
Hélène MJ <helene.jonin@gmail.com>
2024-09-05 11:11:02 +0200

    Refactor validation.ts
    * Better error handling
    * Add typing

--- __tests__/validation.test.ts
+++ __tests__/validation.test.ts
@@ -2,19 +2,21 @@ import {readFileSync} from "node:fs";
 
 import {expect, test} from '@jest/globals';
 
-import {validate} from "../validation";
+import {CodemetaValidationError, validate} from "../validation";
 
-const getCodemeta = (filename: string) =>
-    JSON.parse(readFileSync(`./data/examples/${filename}`, "utf8"));
+const getCodemeta = (suffix: string) =>
+    JSON.parse(readFileSync(`./data/examples/codemeta.${suffix}.json`, "utf8"));
 
 test('empty codemeta fails', async () => {
-  const codemeta = {};
-  const isValid = await validate(codemeta);
-  expect(isValid).toBeFalsy();
+    const codemeta = {};
+    await expect(validate(codemeta))
+        .rejects
+        .toThrow(CodemetaValidationError);
 });
 
 test('minimal codemeta passes', async () => {
-  const codemeta = getCodemeta("codemeta.minimal.json");
-  const isValid = await validate(codemeta);
-  expect(isValid).toBeTruthy();
+    const codemeta = getCodemeta("minimal");
+    await expect(validate(codemeta))
+        .resolves
+        .toBeTruthy();
 });
--- validation.ts
+++ validation.ts
@@ -1,5 +1,6 @@
-import Ajv from "ajv";
+import Ajv, {ErrorObject} from "ajv";
 import addFormats from "ajv-formats"
+// @ts-ignore
 import jsonld from "jsonld"
 
 import {codemetaV3Schema} from "./schemas";
@@ -7,7 +8,14 @@ import {codemetaV3Schema} from "./schemas";
 import codemetaContextV2 from "./data/contexts/codemeta-2.0.json"
 import codemetaContextV3 from "./data/contexts/codemeta-3.0.json"
 
-const CODEMETA_CONTEXTS = {
+class CodemetaValidationError extends Error {
+    constructor(errors: ErrorObject[] | null | undefined) {
+        super(ajv.errorsText(errors));
+        this.name = "CodemetaValidationError";
+    }
+}
+
+const CODEMETA_CONTEXTS: { [Key: string]: {} } = {
     "https://doi.org/10.5063/schema/codemeta-2.0": codemetaContextV2,
     "https://w3id.org/codemeta/3.0": codemetaContextV3
 }
@@ -16,30 +24,28 @@ const ajv = new Ajv({allowUnionTypes: true});
 addFormats(ajv);
 
 const nodeDocumentLoader = jsonld.documentLoaders.node();
-const customLoader = async (url, options) => {
-  if(url in CODEMETA_CONTEXTS) {
-    return {
-      contextUrl: null,
-      document: CODEMETA_CONTEXTS[url],
-      documentUrl: url
-    };
-  }
-  // call the default documentLoader
-  return nodeDocumentLoader(url);
+const customLoader = async (url: string) => {
+    if (url in CODEMETA_CONTEXTS) {
+        return {
+            contextUrl: null,
+            document: CODEMETA_CONTEXTS[url],
+            documentUrl: url
+        };
+    }
+    // call the default documentLoader
+    return nodeDocumentLoader(url);
 };
 jsonld.documentLoader = customLoader;
 
-const validate = async (codemeta, version = "3.0") => {
+const validate = async (codemeta: {}, version = "3.0") => {
     const expanded = await jsonld.expand(codemeta); // Expand with all contexts
     const compacted = await jsonld.compact(expanded, codemetaContextV3); // Compact with codemeta v3.0 context only
     const validateCodemetaV3 = ajv.compile(codemetaV3Schema);
-    if (validateCodemetaV3(compacted)) {
-        console.log(compacted);
-        return true;
-    } else {
-        console.log(validateCodemetaV3.errors);
-        return false;
+    console.log(compacted);
+    if (!validateCodemetaV3(compacted)) {
+        throw new CodemetaValidationError(validateCodemetaV3.errors);
     }
-}
+    return true;
+};
 
-export {validate};
+export {CodemetaValidationError, validate};
